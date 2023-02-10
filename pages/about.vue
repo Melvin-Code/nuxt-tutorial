dd6bb176709f405976f279546aafa22f9af20c70
Melvin Code <melvingonzalez@corporatetools.com>
2023-02-10 11:04:11 -0800

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur sed
+      unde aliquam mollitia suscipit amet nam voluptate, officia debitis, nulla
+      placeat quis quaerat repellat cumque quo velit. Eum, asperiores doloribus!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur sed
+      unde aliquam mollitia suscipit amet nam voluptate, officia debitis, nulla
+      placeat quis quaerat repellat cumque quo velit. Eum, asperiores doloribus!
+    </p>
+    <div>{{ data }}</div>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch("/api/currency/GBP");
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur sed unde aliquam mollitia suscipit amet nam voluptate, officia debitis, nulla placeat quis quaerat repellat cumque quo velit. Eum, asperiores doloribus!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur sed unde aliquam mollitia suscipit amet nam voluptate, officia debitis, nulla placeat quis quaerat repellat cumque quo velit. Eum, asperiores doloribus!</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
